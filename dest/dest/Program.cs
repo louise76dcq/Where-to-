de92df3836e54719b724c0119c63a5cee72e2fc8
louis <louise.ducrocq@viacesi.fr>
2025-03-24 20:40:17 +0100

    deuieme tout

--- dest/dest/Program.cs
+++ dest/dest/Program.cs
@@ -2,29 +2,41 @@ using dest.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuration de Kestrel
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(System.Net.IPAddress.Any, 5000); // Écoute sur toutes les interfaces au port 5000
+});
+
+// Ajout des services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<SqlConnection>();
 builder.Services.AddTransient<DestinationService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policyBuilder => policyBuilder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
-
-// 🔥 Ajoute ceci pour éviter l'erreur
+// Authentification et autorisation
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 
-
 var app = builder.Build();
 
-
+// Configuration de l'application
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication(); // Ajoute l'authentification
-app.UseAuthorization();  // Ajoute l'autorisation
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
